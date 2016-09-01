f908ed4eeb4d1a2f104cbe5d27ad1708a11cb879
jovyb <jovy.batul@macquarie.com>
2016-09-01 18:45:56 +0800

    Package, File, Service exercise
    Package, File, Service exercise

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -9,7 +9,7 @@ file {'/etc/sysconfig/memcached':
    owner => 'root',
    group => 'root',
    mode => '0644',
-   source => 'puppet:///modules/memcached/memcached',
+   require => Package['memcached'],
 }
 
 service {'memcached':
