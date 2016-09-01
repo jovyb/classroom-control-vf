ab95cd633ab1ea0eb2b9fa3ec333f94798378c55
jovyb <jovy.batul@macquarie.com>
2016-09-01 17:00:40 +0800

    remove classification
    remove classification

--- manifests/site.pp
+++ manifests/site.pp
@@ -38,11 +38,11 @@ ini_setting { 'random ordering':
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
-node default {
+# node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  notify { "Hello, my name is ${::hostname}": }
+#  notify { "Hello, my name is ${::hostname}": }
   
   # This is for the Homework 7.3 
   host {'testing.puppetlabs.vm':
@@ -64,4 +64,4 @@ node default {
     }
    include users 
    include skeleton
-}
+# }
