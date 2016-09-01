ff3a134703445d647d108a72f39c2c7c1957c507
jovyb <jovy.batul@macquarie.com>
2016-09-01 18:28:45 +0800

    Package, File, Service exercise
    Package, File, Service exercise

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,25 @@
+class memcached {
+
+package {'memcached':
+   ensure => present,
+}
+
+file {'/etc/sysconfig/memcached':
+   ensure => file,
+   owner => 'root',
+   group => 'root',
+   mode => '0644',
+   port => "11211",
+   user => "memcached",
+   maxconn => "96",
+   cachesize => "32",
+   options => "",
+}
+
+service {'memcached':
+   ensure => present,
+   enable => true,
+   require => File['/etc/sysconfig/memcached'],
+}
+
+}
