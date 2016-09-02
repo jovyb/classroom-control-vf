53de4b2670fd844aebc0ddb5a4f569a2a4c1b0b1
jovyb <jovy.batul@macquarie.com>
2016-09-02 22:29:51 +0800

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,40 +1,12 @@
 class nginx (
-  $root = undef,
-){
-  case $::osfamily {
-    'redhat','debian' : {
-      $package = 'nginx'
-      $owner   = 'root'
-      $group   = 'root'
-      #$docroot = '/var/www'
-      $default_docroot = '/var/www'
-      $confdir = '/etc/nginx'
-      $logdir  = '/var/log/nginx'
-    }
-    'windows' : {
-      $package = 'nginx-service'
-      $owner   = 'Administrator'
-      $group   = 'Administrators'
-      #$docroot = 'C:/ProgramData/nginx/html'
-      $default_docroot = 'C:/ProgramData/nginx/html'
-      $confdir = 'C:/ProgramData/nginx'
-      $logdir  = 'C:/ProgramData/nginx/logs'
-    }
-    default   : {
-      fail("Module ${module_name} is not supported on ${::osfamily}")
-    }
-  }
-    $docroot = $root ? {
-    undef => $default_docroot,
-    default => $root,
-    }
-    
-  # user the service will run as. Used in the nginx.conf.erb template
-  $user = $::osfamily ? {
-    'redhat'  => 'nginx',
-    'debian'  => 'www-data',
-    'windows' => 'nobody',
-  }
+  $package = $nginx::params::package,
+  $owner   = $nginx::params::owner,
+  $group   = $nginx::params::group,
+  $docroot = $nginx::params::docroot,
+  $confdir = $nginx::params::confdir,
+  $logdir  = $nginx::params::logdir,
+  $user    = $nginx::params::user,
+) inherits nginx::params {
 
   File {
     owner => $owner,
@@ -47,26 +19,31 @@ class nginx (
   }
 
   file { [ $docroot, "${confdir}/conf.d" ]:
-    ensure => directory,
+    ensure  => directory,
+    require => Package['nginx'],
   }
 
   file { "${docroot}/index.html":
     ensure => file,
     source => 'puppet:///modules/nginx/index.html',
   }
+  
   file { "${confdir}/nginx.conf":
     ensure  => file,
     content => template('nginx/nginx.conf.erb'),
+    require => Package['nginx'],
     notify  => Service['nginx'],
   }
+  
   file { "${confdir}/conf.d/default.conf":
     ensure  => file,
     content => template('nginx/default.conf.erb'),
+    require => Package['nginx'],
     notify  => Service['nginx'],
   }
 
   service { 'nginx':
-    ensure    => running,
-    enable    => true,
+    ensure => running,
+    enable => true,
   }
 }
